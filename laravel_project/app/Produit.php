4e7fb0e7ee25aeb843d84129e5755cc95083d904
gouttebroze <55590605+gouttebroze@users.noreply.github.com>
2022-04-27 19:39:50 +0200

    Update Produit.php

--- laravel_project/app/Produit.php
+++ laravel_project/app/Produit.php
@@ -9,9 +9,9 @@ class Produit extends Model
     protected $fillable = ['title', 'subtitle', 'description', 'price','image', 'year', 'category_id'];
 
     public function getPrice(){
-        //on veux diviser le prix par cent (avec "$this->price" on a accès au prix, on le divise par cent comme il est en cents, on le stocke ds 1 variable "$price")
+        //diviser prix, $this->price, stocke ds var $price
         $price = $this->price / 100;
-        //on retourne un fonction PHP qui permet de formater un nombre, on lui passe le prix, le nbre de décimal après la virgule, on sépare les milliers avec un espace  enfin on fait une concaténation pour les euros
+        //retour fonction de formatage (prix + nbre décimales)
         return number_format($price, 2, ',',' ') . ' €';
     }
 }
